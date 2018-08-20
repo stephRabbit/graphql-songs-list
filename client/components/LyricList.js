de9eb8566409b27d70910e54db1aadd36a1893af
Steph <N/A>
2018-08-19 22:50:59 -0400

    Lyric list content added

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react'
 
 export class LyricList extends Component {
   render() {
+    const { lyrics } = this.props
+    if (lyrics.length === 0) {
+      return <h5>Add some lyrics...</h5>
+    }
+
     return (
       <ul className="collection">
-        <li className="collection-item">Lyrics!!!</li>
+        {lyrics.map(lyric => (
+          <li
+            className="collection-item"
+            key={lyric.id}
+          >
+            {lyric.content}
+          </li>
+        ))}
       </ul>
     )
   }
